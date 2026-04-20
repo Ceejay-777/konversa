592351e1687bd185690b536c289926ed93a600ae
Ceejay-777 <covenantcrackslord01@gmail.com>
2026-04-20 17:28:32 +0100

    chore: health

--- konversa/urls.py
+++ konversa/urls.py
@@ -1,11 +1,14 @@
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from accounts.views import health
 
 
 urlpatterns = [
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='redoc'),
     path('api/raw/', SpectacularAPIView.as_view(), name='schema'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='swagger-ui'),
+    
+    path("health/", health, name="health"),
     
     path("api/auth/", include("accounts.urls")),
     path("api/products/", include("products.urls")),
